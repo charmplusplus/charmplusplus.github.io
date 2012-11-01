9116372707747587ac4b239bddec353df2272cd1
Ramprasad Venkataraman <ramv@illinois.edu>
2012-11-01 16:22:31 -0500

    floating menu working correctly

--- content/assets/prettify.js
+++ content/assets/prettify.js
@@ -1,22 +1,22 @@
-// Grab all code snippets and paint it to highlight syntax
 $(document).ready( function() {
+    // Grab all code snippets and paint it to highlight syntax
     $("pre code").each( function(idx) {
         CodeMirror.runMode($(this).text(), "text/x-charm++", $(this).get(0));
     })
     .addClass("cm-s-lesser-dark")
     .children("span.cm-charmkeyword").css("color", "#dd5ef3");
 
+    // Enable top pull-down menu functionality
     $(".navigation")
     .append('<span id="pulldowntab" class="navsymbol">&raquo;</span>')
     .click( function() { $("ul.manual-toc").fadeToggle(); $("#pulldowntab").toggle(); } )
     .mouseleave( function() { $("ul.manual-toc").fadeOut('slow'); $("#pulldowntab").fadeIn('slow'); } )
     .css('cursor','pointer');
 
-		$("#nav-floatmenu > li")
-		.click(      function() { $(this).css("border-color", "#00a4e4").children('div').fadeToggle(); } )
-		.mouseleave( function() { $(this).children('div').fadeOut().parent().css("border-color", "#ffffff"); } );
-		//$("#nav-floatmenu > li > div")
-		//.mouseleave( function() { $(this).fadeOut().parent().css("border-color", "#ffffff"); } );
+    // Enable floating navigation menu functionality
+	$("#nav-floatmenu > li")
+	.mouseenter( function() { $(this).css("border-color", "#00a4e4").children('div').show(); } )
+	.mouseleave( function() { $(this).css("border-color", "#ffffff").children('div').hide(); } );
 
 } )
 
