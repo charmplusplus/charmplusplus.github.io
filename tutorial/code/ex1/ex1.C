7c6307a3b482954c5697d827baeeecad9030c8a6
Eric Mikida <epmikida@gmail.com>
2015-01-13 15:02:49 -0600

    Adds a first draft of comments to example 1

--- tutorial/code/ex1/ex1.C
+++ tutorial/code/ex1/ex1.C
@@ -1,7 +1,11 @@
 #include "ex1.h"
 
+// This is the main entry point of the program.
 Main::Main(CkArgMsg* m) {
   CkPrintf("Main chare created...\n");
+
+  // Here we create a new Greeter chare and send it a message. More explanation
+  // will come later in the tutorial.
   CProxy_Greeter greeter = CProxy_Greeter::ckNew();
   greeter.sayHello();
 }
@@ -10,8 +14,12 @@ Greeter::Greeter() {
   CkPrintf("Greeter created...\n");
 }
 
+// This is the definition of the sayHello entry method, which is called remotely
+// by the main chares constructor.
 void Greeter::sayHello() {
+  // CkPrintf is a charm specific version of printf.
   CkPrintf("Hello world!\n");
+  // CkExit() exits the program on every single core it is running on.
   CkExit();
 }
 
--- tutorial/code/ex1/ex1.h
+++ tutorial/code/ex1/ex1.h
@@ -1,11 +1,15 @@
+// Each module generates some declarations in <module name>.decl.h. These must
+// be included before declarations of the chare classes.
 #include "ex1.decl.h"
 
+// Every class declaration of a chare must inherit from the generated base
+// class for that chare, CBase_<chare name>.
 class Main : public CBase_Main {
  public:
   Main(CkArgMsg*);
 };
 
-class Greeter {
+class Greeter : public CBase_Greeter {
  public:
   Greeter();
   void sayHello();
